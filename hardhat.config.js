135fc6365142ca9697c5dc403dca64d83bb534e1
rajsha10 <rajsha8617@gmail.com>
2024-08-31 15:41:28 +0530

    final commit submit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,19 +1,27 @@
-require('@nomiclabs/hardhat-ethers');
-require('@nomiclabs/hardhat-etherscan');
-require('dotenv').config();
-
-const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.19",
+  paths: {
+    artifacts: "./client/src",
+  },
   networks: {
-    arbitrum: {
-      url: `https://arb1.arbitrum.io/rpc`,
-      accounts: [`0x${PRIVATE_KEY}`],
+    opencampus: {
+      url: `https://rpc.open-campus-codex.gelato.digital/`,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY],
     },
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    customChains: [
+      {
+        network: "opencampus",
+        chainId: 656476,
+        urls: {
+          apiURL: "https://opencampus-codex.blockscout.com/api",
+          browserURL: "https://opencampus-codex.blockscout.com",
+        },
+      },
+    ],
   },
 };
